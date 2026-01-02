80517afa8ee751ce790986252688d1be77cbfe16
xiaopange <2361834519@qq.com>
2026-01-02 23:46:14 +0800

    feat: 更新主题，增加用户需求规格说明书

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,20 +5,20 @@ import DefaultTheme from 'vitepress/theme'
 // import './style.css'
 import './custom.css' // 自定义样式，演示修改主题色
 
-// export default {
-//   extends: DefaultTheme,
-//   Layout: () => {
-//     return h(DefaultTheme.Layout, null, {
-//       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-//     })
-//   },
-//   enhanceApp({ app, router, siteData }) {
-//     // ...
-//   }
-// } satisfies Theme
-
-
 export default {
+  extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+    })
+  },
+  enhanceApp({ app, router, siteData }) {
+    // ...
+  }
+} satisfies Theme
+
+
+// export default {
    
-  ...DefaultTheme
-};
+//   ...DefaultTheme
+// };
